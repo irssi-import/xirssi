9189f6f282fa6b12dc82f482e53bd11670802624
Timo Sirainen <cras@irssi.org>
2002-04-27 21:04:57 +0000

    gtk-2.0.m4 updates..

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,10 @@
-aclocal $ACLOCAL_FLAGS
+# did we have gtk-2.0.m4?
+error=`aclocal $ACLOCAL_FLAGS 2>&1`
+if test "x`echo $error|grep AM_PATH_GTK`" != "x"; then
+  cp gtk-2.0.m4_ gtk-2.0.m4
+  aclocal $ACLOCAL_FLAGS
+fi
+
 autoheader
 autoconf
 automake --add-missing
