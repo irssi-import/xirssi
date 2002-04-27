0785dfac7ff5f44323799a81eaef50c1cdf608c4
Timo Sirainen <cras@irssi.org>
2002-04-27 14:53:13 +0000

    whops, generate.sh wasn't needed :)

--- autogen.sh
+++ autogen.sh
@@ -2,7 +2,3 @@ aclocal $ACLOCAL_FLAGS
 autoheader
 autoconf
 automake --add-missing
-
-cd pixmaps
-./generate.sh
-cd ..
--- pixmaps/generate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for i in *.png; do
-  echo "static const unsigned char `echo $i|sed -e 's/-/_/' -e 's/.png$//'`[] = {" > $i.h
-  cat $i|perl -e 'undef $/; $data = <>; for (0 .. length($data)-1) { print ord(substr($data, $_, 1)).", "; }' | sed 's/, $//' >> $i.h
-  echo "};" >> $i.h
-done
